0741aa894051df7d4c71a6a37ce77690c02a11e5
margo <f411inv41@gmail.com>
2023-02-27 23:30:56 +0200

    resolved some errors

--- Pong/shape.hpp
+++ Pong/shape.hpp
@@ -13,7 +13,7 @@ public:
     Shape(olc::PixelGameEngine* pge, olc::vf2d pos, olc::vf2d dir, int speed);
 
     //virtual void draw() = 0;
-    virtual void move() = 0;
+    //virtual void move() = 0;
 };
 
 
